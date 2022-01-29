c0f510367e65cc95b1f9e6e51390a732e6d5753b
rushika.shrestha <rushika.shrestha@braindigit.com>
2022-01-29 13:42:34 +0530

    recent update

--- App/urls.py
+++ App/urls.py
@@ -2,7 +2,11 @@ from django.urls import path,include
 
 from .views import Flashcards
 app_name="App"
+from .views import Flashcards,Introduction,Recommender,getInfo
 
 urlpatterns=[
-    path('Cards/', Flashcards),
+    path('Communication/',Flashcards,name="Communication"),
+    path('',Introduction),
+    path('Recommender/',Recommender,name="Recommender"),
+    path('Tips/<str:search>',getInfo,name="Advice"),
 ]
--- Voiceproject/urls.py
+++ Voiceproject/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import Flashcards,Introduction
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Communication/',Flashcards),
-    path('',Introduction),
+
+    path('',include('App.urls',namespace='App')),
+    
     
 ]
